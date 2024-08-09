4898d717c8abfc65deda6d323cab5174e770644e
Syed Shazeedul Islam <syedshazeedul@gmail.com>
2024-08-09 14:34:42 +0600

    env key change

--- src/Config/recaptchaV3.php
+++ src/Config/recaptchaV3.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'site_key' => env('LOOM_RECAPTCHA_V3_SITE_KEY', ''),
-    'secret_key' => env('LOOM_RECAPTCHA_V3_SECRET_KEY', ''),
+    'site_key' => env('RECAPTCHA_V3_SITE_KEY', ''),
+    'secret_key' => env('RECAPTCHA_V3_SECRET_KEY', ''),
 ];
