9906b9781582650abf1fd98387b5a3f5a252251b
shazeedul <syedshazeedul@gmail.com>
2024-08-10 14:02:22 +0600

    rules invoke

--- src/Rules/RecaptchaV3Rule.php
+++ src/Rules/RecaptchaV3Rule.php
@@ -5,16 +5,13 @@ namespace Shazeedul\RecaptchaV3\Rules;
 use Illuminate\Contracts\Validation\Rule;
 use Shazeedul\RecaptchaV3\Facades\RecaptchaV3;
 
-class RecaptchaV3Rule implements Rule
+class RecaptchaV3Rule
 {
-    public function passes($attribute, $value)
+    public function __invoke($attribute, $value, $fail)
     {
         $response = RecaptchaV3::validate($value);
-        return $response['success'] && $response['score'] >= 0.5; // Customize score threshold as needed
-    }
-
-    public function message()
-    {
-        return 'The reCAPTCHA verification failed.';
+        if (!$response['success'] || $response['score'] < 0.5) { // Customize score threshold as needed
+            $fail('The reCAPTCHA verification failed.');
+        }
     }
 }
